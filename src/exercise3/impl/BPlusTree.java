cbebdb5bd8608a2e502355654b3cdd7aaa994cb3
kaadi2000 <aditya.g.2005001@gmail.com>
2024-11-27 06:13:17 +0100

    Because responsible code should be able to handle its own chores!

--- src/exercise3/impl/BPlusTree.java
+++ src/exercise3/impl/BPlusTree.java
@@ -1,5 +1,6 @@
 package exercise3.impl;
 
+//import java.lang.classfile.components.ClassPrinter;
 import java.util.*;
 
 public class BPlusTree<K extends Comparable<? super K>, V> {
@@ -50,8 +51,16 @@ public class BPlusTree<K extends Comparable<? super K>, V> {
 
         @Override
         Node split() {
-            // TODO: Impl.
-            return null;
+            // TODO: Impl. - Done
+            int mid = keys.size() / 2;
+            IndexNode newI = new IndexNode();
+
+            newI.keys.addAll(keys.subList(mid + 1, keys.size()));
+            newI.children.addAll(children.subList(mid + 1, children.size()));
+            keys.subList(mid, keys.size()).clear();
+            children.subList(mid + 1, children.size()).clear();
+
+            return newI;
         }
     }
 
@@ -61,8 +70,18 @@ public class BPlusTree<K extends Comparable<? super K>, V> {
 
         @Override
         Node split() {
-            // TODO: Impl.
-            return null;
+            // TODO: Impl. - Done
+
+            int mid = records.size() / 2;
+            LeafNode newLeaf = new LeafNode();
+
+            newLeaf.records.addAll(records.subList(mid, records.size()));
+            records.subList(mid, records.size()).clear();
+            newLeaf.next = this.next;
+            this.next = newLeaf;
+
+            return newLeaf;
+
         }
 
         @Override
@@ -76,20 +95,186 @@ public class BPlusTree<K extends Comparable<? super K>, V> {
     public BPlusTree() {
         // TODO: Impl.
     }
+    
 
+    private IndexNode findParent(Node current, Node child) {
+        if (current.isLeaf()) {
+            return null;
+        }
+    
+
+        @SuppressWarnings("unchecked")
+        IndexNode index = (IndexNode) current;
+
+
+        for (Node n : index.children) {
+            if (n == child) {
+                return index;
+            }
+
+            if (!n.isLeaf()) {
+                IndexNode possibleParent = findParent(n, child);
+                if (possibleParent != null) {
+                    return possibleParent;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void handleSplit(Node node, IndexNode parent){
+
+        if ((node.isLeaf() && ((LeafNode) node).records.size() > ENTRY_COUNT) ||
+        (!node.isLeaf() && ((IndexNode) node).keys.size() > ENTRY_COUNT)) {
+        
+        Node newNode = node.split();
+        K middleK;
+
+        if (node.isLeaf()) {
+            LeafNode l = (LeafNode) node;
+            LeafNode newLeaf = (LeafNode) newNode;
+            middleK = newLeaf.records.get(0).key;
+
+            if (parent == null) {
+                IndexNode newRoot = new IndexNode();
+                newRoot.keys.add(middleK);
+                newRoot.children.add(l);
+                newRoot.children.add(newLeaf);
+                root = newRoot;
+
+                return;
+            }
+        } else {
+            IndexNode index = (IndexNode) node;
+            IndexNode newIndex = (IndexNode) newNode;
+            middleK = index.keys.remove(index.keys.size() - 1);
+
+            if (parent == null) {
+                IndexNode newRoot = new IndexNode();
+                newRoot.keys.add(middleK);
+                newRoot.children.add(index);
+                newRoot.children.add(newIndex);
+                root = newRoot;
+                return;
+            }
+
+        }
+
+        int pos = Collections.binarySearch(parent.keys, middleK);
+        
+        if (pos < 0) {
+            pos = -pos - 1;
+        }
+
+        parent.keys.add(pos, middleK);
+        parent.children.add(pos + 1, newNode);
+
+        handleSplit(parent, findParent(root, parent));
+        
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     public V insert(K key, V value) {
-        // TODO: Impl.
+        // TODO: Impl. - Done Hopefully?
+
+        if(root == null){
+            root = new LeafNode();
+
+        }
+
+        Node current = root;
+        IndexNode parent = null;
+
+        while(!current.isLeaf()){
+            parent = (IndexNode) current;
+            int position = Collections.binarySearch(parent.keys, key);
+            if(position < 0){
+                position =- position - 1;
+            }
+
+            current = parent.children.get(position);
+        }
+
+        LeafNode leaf = (LeafNode) current;
+
+        for(Record r : leaf.records){
+            if(r.key.equals(key)){
+                V oValue = r.value;
+                r.value = value;
+                return oValue;
+            }
+        }
+
+        Record newR = new Record(key);
+        newR.setValue(value);
+
+        int position = Collections.binarySearch(leaf.records, newR);
+        if(position < 0){
+            position =- position - 1;
+        }
+
+        leaf.records.add(position, newR);
+
+        handleSplit(leaf, parent);
+
         return null;
     }
 
     public V pointQuery(K key) {
-        // TODO: Impl.
-        return null;
+        // TODO: Impl. - Done
+
+        Node current = root;
+
+        while (!current.isLeaf()) {
+
+            @SuppressWarnings("unchecked")
+            IndexNode i = (IndexNode) current;
+            int pos = Collections.binarySearch(i.keys, key);
+            if (pos < 0) {
+                pos = -pos - 1;
+            }
+            current = i.children.get(pos);
+        }
+
+        @SuppressWarnings("unchecked")
+        LeafNode leaf = (LeafNode) current;
+        int pos = Collections.binarySearch(leaf.records, new Record(key));
+        return (pos >= 0) ? leaf.records.get(pos).getValue() : null;
     }
 
     public List<? extends Map.Entry<K, V>> rangeQuery(K minKey, K maxKey) {
-        // TODO: Impl.
-        return List.of();
+        // TODO: Impl. - Done
+        List<Map.Entry<K, V>> results = new ArrayList<>();
+        Node current = root;
+
+        while (!current.isLeaf()) {
+            @SuppressWarnings("unchecked")
+            IndexNode index = (IndexNode) current;
+            int pos = Collections.binarySearch(index.keys, minKey);
+            if (pos < 0) {
+                pos = -pos - 1;
+            }
+            current = index.children.get(pos);
+        }
+
+        @SuppressWarnings("unchecked")
+        LeafNode leaf = (LeafNode) current;
+        while (leaf != null) {
+            for (Record r : leaf.records) {
+                if (r.key.compareTo(minKey) >= 0 && r.key.compareTo(maxKey) <= 0) {
+                    results.add(r);
+                } else if (r.key.compareTo(maxKey) > 0) {
+                    return results;
+                }
+            }
+
+            leaf = leaf.next;
+        }
+
+        return results;
     }
 
     public static void main(String[] args) {
